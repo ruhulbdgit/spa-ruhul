0e87be1008f6a00603a08aa9c3fd7d0b213daee0
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2023-08-01 22:41:16 +0600

    testing

--- spa-ruhul/process_registration.php
+++ spa-ruhul/process_registration.php
@@ -1,5 +1,15 @@
-first name
-last name
-email
-password
-regiser .
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Process</title>
+</head>
+
+<body>
+    <H2>U are most welcome</H2>
+
+</body>
+
+</html>
