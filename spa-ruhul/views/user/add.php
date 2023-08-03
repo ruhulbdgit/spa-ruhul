9b57d2870f7e30e542fe58300f013784da6795bb
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2023-08-03 15:52:55 +0600

    just br tag add

--- spa-ruhul/views/user/add.php
+++ spa-ruhul/views/user/add.php
@@ -3,17 +3,17 @@ first name ,last name , email , password , regiser .
 */ -->
 
 <form action="process_registration.php" method="post">
-    <label for="first_name">First Name:</label>
+    <label for="first_name">First Name:</label> <br />
     <input type="text" id="first_name" name="first_name" required><br>
 
-    <label for="last_name">Last Name:</label>
+    <label for="last_name">Last Name:</label><br />
     <input type="text" id="last_name" name="last_name" required><br>
 
-    <label for="email">Email:</label>
+    <label for="email">Email:</label><br />
     <input type="email" id="email" name="email" required><br>
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required><br>
+    <label for="password">Password:</label><br />
+    <input type="password" id="password" name="password" required><br><br />
 
 
 
